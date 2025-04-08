bd8f046932655edf144bf1d21280bca57b5a00d9
unknown <system-git-kato@wv21.co.jp>
2025-04-08 14:37:25 +0900

    test

---
+++ sample2.js
@@ -0,0 +1,21 @@
+function　func1() {
+    return new Promise((resolve) => {
+        setTimeout(()=>{
+            console.log("HelloWorld") ;
+            resolve(true);
+        },1000);
+    })
+}
+function func2(val) {
+    console.log("GoodMorning", val);
+}
+
+function func3(){
+    return func1().then((value) => {
+        func2(value);
+    });
+}
+
+func3();
+
+
