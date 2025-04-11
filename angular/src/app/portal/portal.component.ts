2c211add3562fae461dd288b5efbf1901919938b
unknown <system-git-kato@wv21.co.jp>
2025-04-11 10:59:57 +0900

    0411am

--- angular/src/app/portal/portal.component.ts
+++ angular/src/app/portal/portal.component.ts
@@ -21,8 +21,16 @@ public names= new Array;
 
  ngOnInit(): void {
    this.name = "KATO";
-   this.names = ["Ono" , "Yono","Masuda"];
-
+   this.names = [{number:0, name:"Ono" }, 
+                  {number:1, name:"Yono"},
+                  {number:2, name:"Masuda"}, 
+                  {number:3, name:"Watanabe"},
+                  {number:4, name:"Horie"}
+                ]; //項目追加
+   for (let i = 0; i < this.names.length; i++) { //最大値変えないと →lengthにしてみよう
+   console.log (this.names[i].name); //0番の名前部分を抽出
+  
+   }
  }
 
 //追加
