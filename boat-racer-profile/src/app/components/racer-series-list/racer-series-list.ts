37c81cfdc7891b89076f716be34c6a5adcfb2ab8
yuya-kato33 <system-git-kato@wv21.co.jp>
2026-04-30 17:38:35 +0900

    ページ送りok

--- boat-racer-profile/src/app/components/racer-series-list/racer-series-list.ts
+++ boat-racer-profile/src/app/components/racer-series-list/racer-series-list.ts
@@ -1,7 +1,8 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Racer } from '../../services/racer.service';
 import { SeriesService } from '../../services/series.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-racer-series-list',
@@ -10,22 +11,59 @@ import { SeriesService } from '../../services/series.service';
   templateUrl: './racer-series-list.html',
   styleUrls: ['./racer-series-list.css']
 })
-export class RacerSeriesList implements OnChanges {
+export class RacerSeriesList implements OnInit, OnChanges {
   @Input() startDate = '';
   @Input() jcd = '';
 
+  // グリッドページング
   racers: Racer[] = [];
+  pages: Racer[][] = [];
+  currentPage = 0;
+  pageSize = 30;
+  cols = 6;
+  rows = 5;
+  requestedPage = 0;
+
   isLoading = false;
   error: string | null = null;
 
-  cols = 6;
+  constructor(private seriesService: SeriesService, private route: ActivatedRoute) { }
 
-  constructor(private seriesService: SeriesService) { }
+  // URL処理
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      const p = Number(params['page']);
+      if (!isNaN(p)) {
+        this.requestedPage = p;
+      }
+    })
+  }
 
+  // データ取得
   ngOnChanges(): void {
     if (this.startDate && this.jcd) {
       this.fetchRacersBySeries();
     };
+  }
+
+  // ページング関連関数
+  get pagedRacers(): Racer[] {
+    return this.pages[this.currentPage] ?? [];
+  }
+
+  get totalPages(): number {
+    return this.pages.length;
+  }
+
+  // ページ切り替え関数
+  setPage(pageIndex: number): void {
+    if (pageIndex < 0 || pageIndex >= this.pages.length) return;
+    this.currentPage = pageIndex;
+  }
+
+  nextPage(): void {
+    if (this.pages.length <= 1) return;
+    this.currentPage = (this.currentPage + 1) % this.pages.length;
   }
 
   fetchRacersBySeries() {
@@ -35,6 +73,18 @@ export class RacerSeriesList implements OnChanges {
       next: (data) => {
         console.log('節ごとの選手データ', data);
         this.racers = data;
+
+        // this.currentPage = 0;
+
+        this.pages = [];
+        for (let i = 0; i < data.length; i += this.pageSize) {
+          this.pages.push(data.slice(i, i + this.pageSize));
+        }
+
+        // ここが鳥獣用
+        if (this.pages.length > 0) {
+          this.currentPage = Math.min(this.requestedPage, this.pages.length - 1);
+        }
         this.isLoading = false;
       },
       error: (err) => {
@@ -61,4 +111,6 @@ export class RacerSeriesList implements OnChanges {
   normalizeShibu(shibu: string): string {
     return shibu.replace(/\s+/g, '').replace(/　/g, ''); // 半角・全角空白を削除
   }
+
+
 }
